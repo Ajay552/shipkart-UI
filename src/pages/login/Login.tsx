1be6227ca68d7074b84dde5c830502160e8fa436
ajay_s <sajayyy01234@gmail.com>
2025-01-20 10:35:55 +0530

    feat: integrate ShadCN UI components

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Login = () => {
+    return (
+        <div>
+            Login
+            <Button size={"lg"}>Login</Button>
+        </div>
+    )
+}
+
+export default Login;
